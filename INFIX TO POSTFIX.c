c6c408ba92d73913087c7f0e43952f37cf766abe
RAJARAM REGHURAM <61812107+rajaram-reghuram@users.noreply.github.com>
2021-01-31 20:37:02 +0530

    INFIX TO POSTFIX.c

--- INFIX TO POSTFIX.c
+++ INFIX TO POSTFIX.c
@@ -1,63 +1,46 @@
-#include <stdio.h>
-#include<stdlib.h> 
-#include<ctype.h>     /* for isdigit(char ) and isalpha(char )*/
+#include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
+
 #define N 100
-char S[N];
+
 int TOP = -1;
+char S[N];
 
-
-
-void PUSH(char ITEM)/* Function for pushing*/
-    {
-       if(TOP >= N-1)
-       {
-        printf("\nStack is Full!!!\n");
-       }
-      else
-     {
-      TOP++;
-      S[TOP] = ITEM;
-    }
-}
-/*Function for poping */
-
-char POP() 
-    {
-        char ITEM;
-        
-       if (TOP < 0)
-       {
-        printf("\nStack is Empty!!!\n");
-        
-       }
-        else
-        {
-            ITEM=S[TOP];
-            TOP--;
-            return ITEM;
-        }
-    }
-    
-int oper(char symbol)
+void PUSH(char ITEM)
 {
-    if (symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-'||symbol == '('  || symbol == ')'   )
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-int priority(char symbol)
-{
-    if(symbol == '*' || symbol == '/')
+	if(TOP >= N-1)
 	{
-		return 2;
+		printf("\nStack Overflow.");
 	}
-	else if(symbol == '+' || symbol == '-')          
+	else
+	{
+		TOP = TOP+1;
+		S[TOP] = ITEM;
+	}
+}
+
+char POP()
+{
+	char ITEM ;
+
+	if(TOP <0)
+	{
+		printf("\nStack Underflow!");
+		exit(1);
+	}
+	else
+	{
+		ITEM = S[TOP];
+		TOP = TOP-1;
+		return(ITEM);
+	}
+}
+
+int oper(char op)
+{
+	if(op == '*' ||  op == '/' || op == '+' || op == '-')
 	{
 		return 1;
 	}
@@ -67,98 +50,79 @@ int priority(char symbol)
 	}
 }
 
-void InToPost( char infix[N],char postfix[N])
+int precedence(char op)
 {
-   
-    char ITEM,X;
-    int i=0,j=0;
-    
-    PUSH('(');                               /* push '(' onto stack */
-	strcat(infix,")");
-   
-    
-    ITEM = infix[i];
-    
-    while (ITEM != '\0')
-    {
-        if (ITEM = '(')
-        {
-           PUSH(ITEM);
-        }
-        else if(ITEM == ')')         /* if current symbol is ')' then */
+	if(op == '*' || op == '/')
+	{
+		return 2;
+	}
+	else if(op == '+' || op == '-')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void infixToPostfix(char infix[], char postfix[])
+{
+	char X, ITEM;
+	int i = 0, j = 0;
+	PUSH('(');
+	strcat(infix, ")");
+	ITEM = infix[i];
+	while(ITEM != '\0')
+	{
+		if(ITEM == '(')
 		{
-			X=POP();                   /* pop and keep popping until '(' encounterd */
-			while(X != '(')            
+			PUSH(ITEM);
+		}
+		else if(isdigit(ITEM) || isalpha(ITEM))
+		{
+			postfix[j] = ITEM;
+			j++;
+		}
+		else if(oper(ITEM) == 1)
+		{
+			X = POP();
+			while(oper(X) == 1 && precedence(ITEM) <= precedence(X))
+			{
+				postfix[j] = X;
+				j++;
+				X = POP();
+			}
+			PUSH(X);
+			PUSH(ITEM);
+		}
+		else if(ITEM == ')')
+		{
+			X = POP();
+			while(X != '(')
 			{
 				postfix[j] = X;
 				j++;
 				X = POP();
 			}
 		}
-        else if(isdigit(ITEM) || isalpha(ITEM))
-        {
-            postfix[j]=ITEM;
-            j++;
-        }
-        else if(oper(ITEM)==1)
-        {
-            X=POP();        /* get top element of stack as X*/
-            while(oper(X)==1 && priority(ITEM)<= priority(X))  /* if top element is a symbol and its priority is greater than that of the ITEM */
-            {
-               
-                postfix[j]=X;
-                j++;
-                 X = POP();
-            }
-            PUSH(X);
-            PUSH(ITEM);
-        }
-       
-		
 		else
-		{ /* not operand and not operator */
-			printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
-			getchar();
+		{
+			printf("\nInvalid expression!");
 			exit(1);
 		}
-		
 		i++;
-		ITEM = infix[i]; 
-       
-
-    }
-    if(TOP>0)
-	{
-		printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
-		getchar();
-		exit(1);
+		ITEM = infix[i];
 	}
-	if(TOP>0)
-	{
-		printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
-		getchar();
-		exit(1);
-	}
-
-    
-    postfix[j] = '\0'; 
-    
+	postfix[j] = '\0';
 }
 
 int main()
 {
-   char infix[N],postfix[N];
-   
-    printf("Enter Infix Expression : ");
-    gets(infix);
-    
-    
-    InToPost(infix,postfix);
-    
-    
-    printf("\nPostfix Expression = \n");
-    puts(postfix);
-
-    return 0;
+	char infix[N], postfix[N];
+	printf("Enter infix expression: ");
+	gets(infix);
+	infixToPostfix(infix, postfix);
+	printf("\nPostfix expression is   %s", postfix);
+	return 0;
 }
-
